e6ddd7fe413a937768c1caf16944c237683cc16c
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-01-31 22:21:25 -0700

    Update links and add active class to navigation link

--- wdd230/scripts/hamburger.js
+++ wdd230/scripts/hamburger.js
@@ -1,8 +1,10 @@
 
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#hamburger');
+const navigationLink = document.querySelector('a');
 
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
+	navigationLink.classList.toggle('active');
 });
