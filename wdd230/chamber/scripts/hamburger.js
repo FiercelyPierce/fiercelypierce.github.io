ab4e64a08975f91380c23020d6e097870e783ba9
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-02-08 21:03:08 -0700

    Update index.html and hamburger.js

--- wdd230/chamber/scripts/hamburger.js
+++ wdd230/chamber/scripts/hamburger.js
@@ -2,7 +2,6 @@
 const mainnav = document.querySelector('.navigation')
 const hambutton = document.querySelector('#hamburger');
 const navigationLink = document.querySelector('header a');
-const applyButton = document.querySelector('.call-to-action');
 
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
