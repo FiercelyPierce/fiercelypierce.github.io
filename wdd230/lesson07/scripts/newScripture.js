3d905968f315e410e445391a82bde1b864018af0
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-02-22 21:57:29 -0700

    Refactored newScripture.js to add functionality for displaying and deleting chapters

--- wdd230/lesson07/scripts/newScripture.js
+++ wdd230/lesson07/scripts/newScripture.js
@@ -1,22 +1,49 @@
 const input = document.querySelector('#favchap');
 const scriptureList = document.querySelector('#list');
 const button = document.querySelector('button');
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
 
 button.addEventListener('click', () => {
     if (input.value != '') {
-        const newListItem = document.createElement('li');
-        const deleteButton = document.createElement('button');
-
-        newListItem.textContent = input.value;
-        deleteButton.textContent = '❌';
-        newListItem.append(deleteButton);
-        scriptureList.append(newListItem);
-
-        deleteButton.addEventListener('click', () => {
-            scriptureList.removeChild(newListItem);
-            input.focus();
-        });
-
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
         input.value = '';
+        input.focus();
     }
 });
+
+function displayList(chapter) {
+    const newListItem = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    newListItem.textContent = chapter;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+    newListItem.append(deleteButton);
+    scriptureList.append(newListItem);
+
+    deleteButton.addEventListener('click', () => {
+        scriptureList.removeChild(newListItem);
+        deleteChapter(scriptureList.textContent);
+        input.focus();
+    });
+}
+
+function setChapterList() {
+    localStorage.setItem('chapters', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('chapters'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item != chapter);
+    setChapterList();
+}
