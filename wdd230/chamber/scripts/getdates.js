aeb60957766a4313a6b71d363a0522bd2a61a113
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-02-27 21:49:10 -0700

    Update discover.html and related styles

--- wdd230/chamber/scripts/getdates.js
+++ wdd230/chamber/scripts/getdates.js
@@ -1,2 +1,22 @@
+if (typeof localStorage !== 'undefined') {
+  const lastVisit = localStorage.getItem('lastVisit');
+  const currentDate = new Date();
+
+  if (!lastVisit) {
+    document.getElementById("sidebarVisit").textContent = "Welcome! Let us know if you have any questions.";
+  } else {
+    const timeDiff = currentDate - new Date(lastVisit);
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (daysDiff < 1) {
+      document.getElementById("sidebarVisit").textContent = "Back so soon! Awesome!";
+    } else {
+      const message = `You last visited ${daysDiff} ${daysDiff === 1 ? 'day' : 'days'} ago.`;
+      document.getElementById("sidebarVisit").textContent = message;
+    }
+  }
+  localStorage.setItem('lastVisit', currentDate);
+}
+
 document.getElementById("currentYear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
