559b23e46f3c1a8b459f2f69bb71ff60a8a960e0
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-03-12 22:36:03 -0600

    Add grid and list buttons to the directory page and implement functionality to switch between grid and list view

--- wdd230/chamber/scripts/members.js
+++ wdd230/chamber/scripts/members.js
@@ -1,10 +1,25 @@
 const linksURL = "data/members.json";
+const gridButton = document.getElementById("grid");
+const listButton = document.getElementById("list");
 
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
+
   displayLinks(data);
 }
+
+gridButton.addEventListener("click", () => {
+  const membersSection = document.querySelector("#linksContainer section");
+  membersSection.classList.remove("list");
+  membersSection.classList.add("grid");
+});
+
+listButton.addEventListener("click", () => {
+  const membersSection = document.querySelector("#linksContainer section");
+  membersSection.classList.remove("grid");
+  membersSection.classList.add("list");
+});
 
 function displayLinks(members) {
   const links = members.members;
@@ -24,12 +39,12 @@ function displayLinks(members) {
     const memberHeading = document.createElement("p");
     memberHeading.textContent = memberObj.membership + " Member";
     const memberAddress = document.createElement("p");
-    memberAddress.textContent = "Address: " + memberObj.address;
+    memberAddress.textContent = memberObj.address;
     const memberPhone = document.createElement("p");
-    memberPhone.textContent = "Phone: " + memberObj.phone;
+    memberPhone.textContent = memberObj.phone;
     const memberWebsite = document.createElement("a");
     memberWebsite.href = memberObj.website;
-    memberWebsite.textContent = "Website: " + memberObj.website;
+    memberWebsite.textContent = memberObj.website;
     const memberImage = document.createElement("img");
     memberImage.src = "images/" + memberObj.image;
     memberImage.alt = memberObj.name;
