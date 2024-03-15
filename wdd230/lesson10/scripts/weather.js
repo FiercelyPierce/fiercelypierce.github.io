4b382bcf74f588cc8fcbf262a11395c0c8f70e6e
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-03-15 15:29:37 -0700

    Update weather API URL and fix displayResults function

--- wdd230/lesson10/scripts/weather.js
+++ wdd230/lesson10/scripts/weather.js
@@ -1,14 +1,14 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/3.0/weather?lat=49.7557&lon=6.6394&appid=d413370813e147d56d9cced57baf9b6f&units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.7557&lon=6.6394&appid=d413370813e147d56d9cced57baf9b6f&units=imperial';
 
 async function apiFetch(url) {
   try {
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
+      displayResults(data);
     }
     else {
       throw Error(await response.text());
@@ -16,6 +16,13 @@ async function apiFetch(url) {
   } catch (error) {
     console.error(error);
   }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg; F`
+  weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  weatherIcon.alt = data.weather[0].description;
+  captionDesc.textContent = data.weather[0].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
 apiFetch(url);
