2f7a39979006a44a6f58b4a35b8419abd49df1ed
Pierce Cirks <97271154+FiercelyPierce@users.noreply.github.com>
2024-03-15 17:34:59 -0700

    Refactor weather section in index.html and base.css

---
+++ wdd230/chamber/scripts/weather.js
@@ -0,0 +1,28 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=33.2487&lon=-111.6343&appid=d413370813e147d56d9cced57baf9b6f&units=imperial';
+
+async function apiFetch(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    }
+    else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg; F`
+  weatherIcon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  weatherIcon.alt = data.weather[0].description;
+  captionDesc.textContent = data.weather[0].description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
+
+apiFetch(url);
